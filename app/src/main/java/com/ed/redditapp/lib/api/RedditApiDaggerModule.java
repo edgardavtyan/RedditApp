c127824e921197b1b0a0a5de2779fcae5228b738
Edgar Davtyan <edgarda.1994@gmail.com>
2021-04-07 03:05:15 +0300

    Add singleton scope to reddit api module

--- app/src/main/java/com/ed/redditapp/lib/api/RedditApiDaggerModule.java
+++ app/src/main/java/com/ed/redditapp/lib/api/RedditApiDaggerModule.java
@@ -1,11 +1,14 @@
 package com.ed.redditapp.lib.api;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class RedditApiDaggerModule {
     @Provides
+    @Singleton
     public RedditApi provideRedditApi() {
         return new RedditApi();
     }
